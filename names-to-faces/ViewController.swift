eb19755c43cf330303e5ca8dd70659e79493418c
Leo Liu <leo.liu@aequilibrium.ca>
2018-07-23 16:58:22 -0700

    using collections view

---
+++ names-to-faces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  names-to-faces
+//
+//  Created by Leo Liu on 7/23/18.
+//  Copyright © 2018 hungryforcookies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
