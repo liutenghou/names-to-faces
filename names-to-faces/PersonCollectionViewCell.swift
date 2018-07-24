dde75bffa6cba367114b1b938deea440500b0cd5
Leo Liu <leo.liu@aequilibrium.ca>
2018-07-24 16:45:38 -0700

    added custom collection cell

---
+++ names-to-faces/PersonCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PersonCollectionViewCell.swift
+//  names-to-faces
+//
+//  Created by Leo Liu on 7/24/18.
+//  Copyright © 2018 hungryforcookies. All rights reserved.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- names-to-faces/ViewController.swift
+++ names-to-faces/ViewController.swift
@@ -20,6 +20,14 @@ class ViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: collection view methods
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:PersonCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCollectionViewCell
+        return cell
+    }
 }
 
